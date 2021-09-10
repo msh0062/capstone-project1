faa9270ee97ea1d7779e6efc647dabb74e351c0c
Mason Hassen <mshassen08@yahoo.com>
2021-09-10 15:37:40 -0500

    Added Header, footer, main, and nav

--- client/capstone-club-reg/src/app/app.component.ts
+++ client/capstone-club-reg/src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { ViewEncapsulation } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'cr-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'capstone-club-reg';
