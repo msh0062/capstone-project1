60459123ca83dcd78ad8c596c5a38f0bef3c62ad
Mason Hassen <mshassen08@yahoo.com>
2021-09-10 17:39:46 -0500

    routed home button

--- client/capstone-club-reg/src/app/app-routing.module.ts
+++ client/capstone-club-reg/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const fallbackRoute: Route = {
+  path: '**', component: HomeComponent
+}
+
+const routes: Routes = [ 
+  {
+    path: '',
+    children: [
+      {path: 'home',  component: HomeComponent},
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
