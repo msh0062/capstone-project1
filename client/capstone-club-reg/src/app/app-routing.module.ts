0e969cfede4bd6a412104de0e7551b2aab7094ce
Mason Hassen <mshassen08@yahoo.com>
2021-09-10 23:33:03 -0500

    update

--- client/capstone-club-reg/src/app/app-routing.module.ts
+++ client/capstone-club-reg/src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Route } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
-const fallbackRoute: Route = {
-  path: '**', component: HomeComponent
-}
+// const fallbackRoute: Route = {
+//   path: '**', component: HomeComponent
+// }
 
 const routes: Routes = [ 
   {
     path: '',
     children: [
-      {path: 'home',  component: HomeComponent},
-
     ]
   }
 ];
